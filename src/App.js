1f30c24ae56e8f61a07ae2a43b4c87bf31668e36
Alexander Whan <whan.alexander@gmail.com>
2020-11-28 17:56:18 -0800

    Moved basic fetch logic from App to Main

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-const getRestaurants = () => {
-  const response = fetch(
-    'https://code-challenge.spectrumtoolbox.com/api/restaurants',
-    {
-      headers: {
-        Authorization: 'Api-Key q3MNxtfep8Gt',
-      },
-    }
-  )
-    .then(res => res.json())
-    .then(data => {
-      console.log('DATA', data);
-      // May need to refactor as async/await
-    });
-};
-
 const App = () => {
-  useEffect(() => {
-    getRestaurants();
-  });
-
   return (
     <>
       <Header />
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import List from '../List';
 
 const Main = () => {
+  const getRestaurants = () => {
+    const response = fetch(
+      'https://code-challenge.spectrumtoolbox.com/api/restaurants',
+      {
+        headers: {
+          Authorization: 'Api-Key q3MNxtfep8Gt',
+        },
+      }
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log('DATA', data);
+        // May need to refactor as async/await
+      });
+  };
+
+  useEffect(() => {
+    getRestaurants();
+  });
+
   return (
     <>
       <h1>Main component!</h1>
