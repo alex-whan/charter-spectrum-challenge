a1e34e3be11644a5084830d86767dee6b838cd7b
Alexander Whan <whan.alexander@gmail.com>
2020-11-29 12:32:18 -0800

    Scaffolded out logic for showing/hiding dropdown menu opts

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { STATES } from './constants/states';
 
 const Dropdown = () => {
@@ -6,13 +6,38 @@ const Dropdown = () => {
   const [isActive, setIsActive] = useState(false);
   const onClick = () => setIsActive(!isActive);
 
+  useEffect(() => {
+    const pageClickEvent = e => {
+      if (
+        dropdownRef.current !== null &&
+        !dropdownRef.current.contains(e.target)
+      ) {
+        setIsActive(!isActive);
+      }
+    };
+    if (isActive) {
+      window.addEventListener('click', pageClickEvent);
+    }
+
+    return () => {
+      window.removeEventListener('click', pageClickEvent);
+    };
+  }, [isActive]);
+
   return (
     <div>
       <button onClick={onClick}>Filter by State</button>
       <nav>
         <ul>
           {STATES.map(state => {
-            return <li key={state.id}>{state.code}</li>;
+            return (
+              <li
+                key={state.id}
+                className={`menu ${isActive ? 'active' : 'inactive'}`}
+              >
+                {state.code}
+              </li>
+            );
           })}
         </ul>
       </nav>
