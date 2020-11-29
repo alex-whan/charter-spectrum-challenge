d4b0867fdd47492148a00bfc74e4600e49da1d77
Alexander Whan <whan.alexander@gmail.com>
2020-11-29 11:34:49 -0800

    Imported state constants from external file

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Table from '../Table';
+import STATES from './constants/states';
 
 const Main = () => {
   const [restaurants, setRestaurants] = useState([]);
+  const states = STATES.STATES;
 
   const getRestaurants = async () => {
     const response = await fetch(
@@ -35,12 +37,19 @@ const Main = () => {
 
   useEffect(() => {
     getRestaurants();
-    console.log('RESTAURANTS??', restaurants);
+    // console.log('RESTAURANTS??', restaurants);
   }, []);
 
   return (
     <>
       <h1>Main component!</h1>
+      <div>
+        <ul>
+          {states.map(state => {
+            return <li key={state.id}>{state.code}</li>;
+          })}
+        </ul>
+      </div>
       <button>Filter by State</button>
       <button>Filter by Genre</button>
       <Table props={restaurants} />
