f7528f8f478cfa96274fb3f3601ca4517e3113c2
Alexander Whan <whan.alexander@gmail.com>
2020-11-29 20:56:41 -0800

    Added useForm custom hook to handle search queries

---
+++ src/hooks/formHook.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+const useForm = callback => {
+  const [values, setValues] = useState({});
+
+  const handleSubmit = e => {
+    if (e) {
+      e.preventDefault();
+    }
+    e.target.reset();
+    callback(values);
+  };
+
+  const handleInputChange = e => {
+    e.persist();
+    setValues(values => ({ ...values, [e.target.name]: e.target.value }));
+  };
+
+  return { handleSubmit, handleInputChange, values };
+};
+
+export default useForm;
